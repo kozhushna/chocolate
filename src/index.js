cbd917c16c98a34dd67ddfdf74a59e46286134b7
kozhushna <k.kozhushna@gmail.com>
2023-09-22 12:02:53 +0300

    Add fonts and Global styles

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Global } from '@emotion/react';
+import { GlobalStyles } from 'styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global styles={GlobalStyles} />
     <App />
   </React.StrictMode>
 );
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,95 @@
+import { css } from '@emotion/react';
+
+import ttfRegular from '../fonts/Montserrat-Regular.ttf';
+import ttfMedium from '../fonts/Montserrat-Regular.ttf';
+import ttfSemiBold from '../fonts/Montserrat-SemiBold.ttf';
+import ttfBold from '../fonts/Montserrat-Bold.ttf';
+
+export const GlobalStyles = css`
+  @font-face {
+    font-family: 'Montserrat';
+    font-weight: 400;
+    font-style: normal;
+    src: url('${ttfRegular}') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-weight: 500;
+    font-style: normal;
+    src: url('${ttfMedium}') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-weight: 600;
+    font-style: normal;
+    src: url('${ttfSemiBold}') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Montserrat';
+    font-weight: 700;
+    font-style: normal;
+    src: url('${ttfBold}') format('truetype');
+  }
+
+  html {
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    width: 100vw;
+  }
+
+  body {
+    width: 100vw;
+    margin: 0;
+    font-family: 'Montserrat', sans-serif;
+    min-height: 100vh;
+
+    font-style: normal;
+    overflow: hidden;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+    padding: 0;
+  }
+
+  ul,
+  ol {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+
+  button {
+    padding: 0;
+    border: none;
+    font: inherit;
+    color: inherit;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
