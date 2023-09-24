26cbec80c93fd8b395e8c5fef1662257585da6bc
kozhushna <k.kozhushna@gmail.com>
2023-09-24 17:15:30 +0300

    Add modern normalize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'modern-normalize';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Global, ThemeProvider } from '@emotion/react';
