90d366bf6570b74dbba8dc2c7c2569fcf7af0c48
RomaBortnik <bortnik25051998@gmail.com>
2023-09-25 13:02:37 +0300

    added utils/products.js

---
+++ src/utils/products.js
@@ -0,0 +1,10 @@
+export const products = [
+  { id: 1, name: 'ORANGE', description: 'Dark chocolate', price: 45 },
+  { id: 2, name: 'APPLE&CRANBERRY', description: 'Milk chocolate', price: 50 },
+  { id: 3, name: 'LIME&SEA SALT', description: 'Dark chocolate', price: 66 },
+  { id: 4, name: 'PINEAPPLE', description: 'Dark chocolate', price: 54 },
+  { id: 5, name: 'CLASSIC', description: 'Milk chocolate', price: 45 },
+  { id: 6, name: 'HONEY', description: 'Milk chocolate', price: 50 },
+  { id: 7, name: 'ROASTED FRUITS', description: 'Dark chocolate', price: 66 },
+  { id: 8, name: 'CLASSIC', description: 'White chocolate', price: 54 },
+];
