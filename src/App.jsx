461ed48c56a5d876abe7233e00256fef3bbb3b3b
AM111D <Dimasssnike@icloud.com>
2023-09-25 02:46:17 -0700

    add_section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Section, Container } from 'App.styled';
-import HowItsMade from './components/HowItsMade/HowItsMade'
+import HowItsMade from './components/HowItsMade/HowItsMade';
+import TasteSensation from 'components/TasteSensation/TasteSensation';
 
 function App() {
   return (
     <main>
       <Section>
         <Container>
+        <TasteSensation/>
+        </Container>
+      </Section>
+      <Section>
+        <Container>
           <p>Hello BITE pull pasha!</p>
           <HowItsMade />
         </Container>
       </Section>
+      
     </main>
   );
 }
