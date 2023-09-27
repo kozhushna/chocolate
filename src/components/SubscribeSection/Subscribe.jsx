1671f14aa5a777fc1325d117ca1f4191cdd8199a
kozhushna <k.kozhushna@gmail.com>
2023-09-27 12:32:13 +0300

    Add text content

--- src/components/SubscribeSection/Subscribe.jsx
+++ src/components/SubscribeSection/Subscribe.jsx
@@ -1,7 +1,11 @@
+import { Container } from 'App.styled';
+import {
+  ContentHolder,
+  SubscribeSection,
+  TextContent,
+  Title,
+} from './Subscribe.styled';
 import { ButtonBgOrange } from 'components/ButtonBgOrange/ButtonBgOrange';
-
-import { Container, Section } from 'App.styled';
-import { Title } from './Subscribe.styled';
 
 export const Subscribe = () => {
   const handleClick = () => {
@@ -9,11 +13,19 @@ export const Subscribe = () => {
   };
 
   return (
-    <Section>
+    <SubscribeSection>
       <Container>
-        <Title>Explore our new chocolate first</Title>
-        <ButtonBgOrange action={handleClick} title={'Subscribe'} />
+        <ContentHolder>
+          <Title>Explore our new chocolate first</Title>
+          <TextContent>
+            From the velvety texture to the rich and complex flavor, our New
+            Chocolate is a true indulgence that will leave you craving more.
+            Made with only the finest ingredients and handcrafted with the
+            utmost care, each piece is a work of art that is sure to please.
+          </TextContent>
+          <ButtonBgOrange action={handleClick} title={'Subscribe'} />
+        </ContentHolder>
       </Container>
-    </Section>
+    </SubscribeSection>
   );
 };
--- src/components/SubscribeSection/Subscribe.styled.js
+++ src/components/SubscribeSection/Subscribe.styled.js
@@ -1,20 +1,59 @@
 import styled from '@emotion/styled';
 
+export const SubscribeSection = styled.section`
+  max-width: 1200px;
+  padding-top: 62px;
+  padding-bottom: 62px;
+  margin: 0 auto;
+  text-align: center;
+  background-color: ${props => props.theme.colors.darkBackground};
+
+  /* @media screen and (${props => props.theme.devices.mobile}) {
+    width: ${props => props.theme.sizes.mobile}px;
+  } */
+
+  @media screen and (${props => props.theme.devices.tablet}) {
+    padding-top: 72px;
+    padding-bottom: 72px;
+  }
+
+  /* @media screen and (${props => props.theme.devices.desktop}) {
+    width: ${props => props.theme.sizes.desktop}px;
+  } */
+`;
+
+export const ContentHolder = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+`;
+
 export const Title = styled.h2`
   /* color: ${props => props.theme.colors.white}; */
-  color: red;
-  text-align: center;
-  font-family: Montserrat;
+  max-width: 291px;
+  color: ${props => props.theme.colors.white};
   font-size: 20px;
-  font-style: normal;
   font-weight: 600;
   line-height: 1.2;
   letter-spacing: -0.6px;
   text-transform: uppercase;
   @media screen and (${props => props.theme.devices.tablet}) {
-    margin-bottom: 50px;
+    max-width: 378px;
     font-size: 24px;
-    line-height: 1, 16;
+    line-height: 1.16;
     letter-spacing: -0.72px;
+  }
+`;
+
+export const TextContent = styled.p`
+  max-width: 326px;
+  color: ${props => props.theme.colors.white};
+  font-weight: 400;
+  letter-spacing: -0.28px;
+  text-align: center;
+
+  @media screen and (${props => props.theme.devices.tablet}) {
+    max-width: 467px;
   }
 `;
