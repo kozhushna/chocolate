2033f2025f14a5cb061f6c89b0170a30acb0b526
Pavlo <113455386+sps-paska-it@users.noreply.github.com>
2023-09-28 11:36:15 +0200

    add modal oder, but not finish, add libary react-hook-form

--- src/components/OderBuy/OderBuy.jsx
+++ src/components/OderBuy/OderBuy.jsx
@@ -1,11 +1,56 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { ButtonClose } from 'components/ButtonIcon/ButtonClose';
-import { StyledDiv } from './OderBuy.styled';
+import {
+  StyledDiv,
+  StyledForm,
+  StyledInput,
+  StyledTextarea,
+  Wrapper,
+  WrapperInput,
+} from './OderBuy.styled';
+import { computeHeadingLevel } from '@testing-library/react';
+import { ClassNames } from '@emotion/react';
+import { ButtonBgOrange } from 'components/ButtonBgOrange/ButtonBgOrange';
 
 export const FormOderBuy = ({ action }) => {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState('');
+
   return (
     <StyledDiv>
-      text
       <ButtonClose action={action} />
+      <StyledForm
+        onSubmit={handleSubmit(data => {
+          console.log(data);
+        })}
+      >
+        <Wrapper>
+          <label htmlFor="firstName">Personal information</label>
+          <WrapperInput>
+            <StyledInput {...register('firstName')} placeholder="Name" />
+            <StyledInput {...register('surname')} placeholder="Surname" />
+          </WrapperInput>
+        </Wrapper>
+        <Wrapper>
+          <label htmlFor="email">Email</label>
+          <StyledInput {...register('email')} placeholder="Enter you email" />
+        </Wrapper>
+        <Wrapper>
+          <label htmlFor="phone">Phone number</label>
+          <StyledInput {...register('phone')} placeholder="Phone number" />
+        </Wrapper>
+        <Wrapper>
+          <label htmlFor="card">Card number</label>
+          <StyledInput {...register('card')} placeholder="Enter card" />
+        </Wrapper>
+        <Wrapper>
+          <label htmlFor="Comment">Comment</label>
+          <StyledTextarea {...register('comment')} placeholder="Enter text" />
+        </Wrapper>
+        {/* <p>{data}</p> */}
+        <ButtonBgOrange title="Submit" />
+      </StyledForm>
     </StyledDiv>
   );
 };
--- src/components/OderBuy/OderBuy.styled.js
+++ src/components/OderBuy/OderBuy.styled.js
@@ -7,3 +7,153 @@ export const StyledDiv = styled(ModalContent)`
     width: 609px;
   }
 `;
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+  margin: 0 auto;
+  padding: 48px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+`;
+
+export const WrapperInput = styled.div`
+  width: 100%;
+  display: flex;
+  column-gap: 22px;
+`;
+
+export const StyledInput = styled.input`
+  width: 100%;
+  border-radius: 13px;
+  padding: 22px;
+  font-size: 18px;
+  /* display: flex;
+  column-gap: 22px; */
+`;
+
+export const StyledTextarea = styled.textarea`
+  width: 100%;
+  border-radius: 13px;
+  padding: 22px;
+  font-size: 18px;
+  /* display: flex;
+  column-gap: 22px; */
+`;
+
+// .p {
+//   color: #bf1650;
+//   text-align: center;
+// }
+
+// textarea,
+// select,
+// input {
+//   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+//     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+//     sans-serif;
+//   display: block;
+//   box-sizing: border-box;
+//   width: 100%;
+//   border-radius: 4px;
+//   border: 1px solid white;
+//   padding: 10px 15px;
+//   margin-bottom: 15px;
+//   font-size: 14px;
+// }
+
+// .label,
+// section > label {
+//   line-height: 2;
+//   text-align: left;
+//   display: block;
+//   margin-bottom: 13px;
+//   margin-top: 20px;
+//   color: white;
+//   font-size: 14px;
+//   font-weight: 200;
+// }
+
+// input[type="submit"],
+// .button {
+//   position: relative;
+//   background: #ec5990;
+//   color: white;
+//   text-transform: uppercase;
+//   border: none;
+//   font-weight: 600;
+//   margin-top: 20px;
+//   padding: 20px;
+//   font-size: 16px;
+//   letter-spacing: 2px;
+//   display: block;
+//   appearance: none;
+//   border-radius: 4px;
+//   width: 100%;
+//   font-weight: 400;
+//   letter-spacing: 0.5rem;
+//   transition: 0.3s all;
+// }
+
+// .buttonBlack {
+//   background: black;
+//   border: 1px solid white;
+// }
+
+// .App {
+//   max-width: 600px;
+//   margin: 0 auto;
+// }
+
+// .container {
+//   display: grid;
+//   grid-template-columns: 1fr 1fr;
+//   grid-gap: 20px;
+// }
+
+// .counter {
+//   font-weight: 400;
+//   background: white;
+//   color: black;
+//   padding: 10px 15px;
+//   border-radius: 4px;
+//   position: fixed;
+//   top: 20px;
+//   right: 30px;
+//   z-index: 9999999;
+// }
+
+// button[type="button"] {
+//   padding: 5px;
+//   background: #516391;
+//   color: white;
+//   letter-spacing: 0px;
+//   text-transform: none;
+//   padding: 10px;
+//   letter-spacing: 1px;
+// }
+
+// input[type="submit"]:hover,
+// button[type="button"]:hover {
+//   background: #bf1650;
+//   color: white;
+// }
+
+// input[type="submit"]:active {
+//   transition: 0.3s all;
+//   top: 2px;
+// }
+
+// .preact {
+//   opacity: 0;
+//   color: white;
+// }
+
+// .preact:hover {
+//   opacity: 1;
+// }
