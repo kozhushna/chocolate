70b9e2946b960b024269749a494cb1682fcf853c
Pavlo <113455386+sps-paska-it@users.noreply.github.com>
2023-09-29 08:43:03 +0200

    delete app test button, connect real button

--- src/components/TopSellers/TopSellers.jsx
+++ src/components/TopSellers/TopSellers.jsx
@@ -23,63 +23,74 @@ import {
   TopSallersFooter,
 } from './TopSellers.styled';
 import { ButtonBgOrange } from 'components/ButtonBgOrange/ButtonBgOrange';
+import { Modal } from 'components/Modal/Modal';
+import { FormOderBuy } from 'components/OderBuy/OderBuy';
 
 export const TopSellers = () => {
-  const { openModal } = useModal(); // test button 'Buy now' and modal, delete
+  const { isModalOpen, openModal, closeModal } = useModal(); // test button 'Buy now' and modal, delete
   return (
-    <Section>
-      <Container>
-        <Title>
-          Top<Accent> Sellers</Accent>
-        </Title>
-        <PictureBox>
-          <picture>
-            <source
-              srcSet={`${img_01_mobile}, 1x, ${img_01_mobile2}, 2x`}
-              media={`(max-width: 767px)`}
-            />
-            <source
-              srcSet={`${img_01_tablet}, 1x, ${img_01_tablet2}, 2x`}
-              media={`(max-width: 1199px)`}
-            />
-            <source
-              srcSet={`${img_01_desktop}, 1x, ${img_01_desktop2}, 2x`}
-              media={`(min-width: 1200px)`}
-            />
-            <ImgStyled src={img_01_mobile} />
-          </picture>
-          <picture>
-            <source
-              srcSet={`${img_02_mobile}, 1x, ${img_02_mobile2}, 2x`}
-              media={`(max-width: 767px)`}
-            />
-            <source
-              srcSet={`${img_02_tablet}, 1x, ${img_02_tablet2}, 2x`}
-              media={`(max-width: 1199px)`}
-            />
-            <source
-              srcSet={`${img_02_desktop}, 1x, ${img_02_desktop2}, 2x`}
-              media={`(min-width: 1200px)`}
-            />
-            <ImgStyled src={img_01_mobile} />
-          </picture>
-          <PictureOnlyDesktop>
-            <source
-              srcSet={`${img_03_desktop}, 1x, ${img_03_desktop2}, 2x`}
-              media={`(min-width: 1200px)`}
-            />
-            <ImgStyled src={img_01_mobile} />
-          </PictureOnlyDesktop>
-        </PictureBox>
-        <TopSallersFooter>
-          <Description>
-            Crazy enough chocolate originates from a bean just like the coffee
-            bean called Cacao. Cacao has been around for thousands of years and
-            was first discovered by the Native American tribe called the Mayans.
-          </Description>
-          <ButtonBgOrange action={openModal} />
-        </TopSallersFooter>
-      </Container>
-    </Section>
+    <>
+      <Section>
+        <Container>
+          <Title>
+            Top<Accent> Sellers</Accent>
+          </Title>
+          <PictureBox>
+            <picture>
+              <source
+                srcSet={`${img_01_mobile}, 1x, ${img_01_mobile2}, 2x`}
+                media={`(max-width: 767px)`}
+              />
+              <source
+                srcSet={`${img_01_tablet}, 1x, ${img_01_tablet2}, 2x`}
+                media={`(max-width: 1199px)`}
+              />
+              <source
+                srcSet={`${img_01_desktop}, 1x, ${img_01_desktop2}, 2x`}
+                media={`(min-width: 1200px)`}
+              />
+              <ImgStyled src={img_01_mobile} />
+            </picture>
+            <picture>
+              <source
+                srcSet={`${img_02_mobile}, 1x, ${img_02_mobile2}, 2x`}
+                media={`(max-width: 767px)`}
+              />
+              <source
+                srcSet={`${img_02_tablet}, 1x, ${img_02_tablet2}, 2x`}
+                media={`(max-width: 1199px)`}
+              />
+              <source
+                srcSet={`${img_02_desktop}, 1x, ${img_02_desktop2}, 2x`}
+                media={`(min-width: 1200px)`}
+              />
+              <ImgStyled src={img_01_mobile} />
+            </picture>
+            <PictureOnlyDesktop>
+              <source
+                srcSet={`${img_03_desktop}, 1x, ${img_03_desktop2}, 2x`}
+                media={`(min-width: 1200px)`}
+              />
+              <ImgStyled src={img_01_mobile} />
+            </PictureOnlyDesktop>
+          </PictureBox>
+          <TopSallersFooter>
+            <Description>
+              Crazy enough chocolate originates from a bean just like the coffee
+              bean called Cacao. Cacao has been around for thousands of years
+              and was first discovered by the Native American tribe called the
+              Mayans.
+            </Description>
+            <ButtonBgOrange action={openModal} />
+          </TopSallersFooter>
+        </Container>
+      </Section>
+      <ButtonBgOrange action={openModal} />
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <FormOderBuy action={closeModal} />
+        </Modal>
+      )}
+    </>
   );
 };
